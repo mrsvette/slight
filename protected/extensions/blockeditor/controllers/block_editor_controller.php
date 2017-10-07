26e34d910419cee202c8a7c27505c786eb06d452
Farid Efendi <mrsvette69@gmail.com>
2017-10-07 21:46:26 +0700

    Define font header request

--- protected/extensions/blockeditor/controllers/block_editor_controller.php
+++ protected/extensions/blockeditor/controllers/block_editor_controller.php
@@ -55,7 +55,15 @@ class BlockEditorController extends BaseController
     public function bundles($request, $response, $args)
     {
         //return $response->withRedirect($this->_settings['params']['site_url'].'/themes/'.$this->_settings['params']['theme'].'/assets/build/bundles/'.$args['name']);
-        return $response->withRedirect($this->getVendorUrl().'bundles/'.$args['name']);
+
+        if (strpos($args['name'], '.woff') !== false) {
+            header('Content-Type: application/x-font-woff');
+        } else {
+            header('Content-Type: application/octet-stream');
+        }
+        $fh = file_get_contents($this->getVendorUrl().'bundles/'.$args['name']);
+
+        return $fh;
     }
 
     public function update($request, $response, $args)
