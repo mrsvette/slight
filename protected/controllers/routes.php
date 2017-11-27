45b3b2fbdad37a9112b0399834b48bcfb63698da
Farid Efendi <mrsvette69@gmail.com>
2017-11-27 20:43:27 +0700

    Niaga redirection method

--- protected/controllers/routes.php
+++ protected/controllers/routes.php
@@ -9,6 +9,10 @@ foreach(glob($settings['settings']['basePath'] . '/extensions/*/controllers/rout
     require_once $ext_routes;
 }
 
+$app->get('/niagahoster', function ($request, $response, $args) {
+    return $response->withRedirect( 'https://goo.gl/V3dpJU' );
+});
+
 $app->get('/[{name}]', function ($request, $response, $args) {
     
 	if (empty($args['name']))
