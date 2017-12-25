0ddc614b9e87d8a3d5b50bf6ddeb0f24249edd7b
Farid Efendi <mrsvette69@gmail.com>
2017-12-25 14:50:45 +0700

    Tinymce editor for posts (#48)
    * Tinymce editor for posts
    
    * Direct uploading image from wysiwyg

--- protected/extensions/blog/controllers/posts_controller.php
+++ protected/extensions/blog/controllers/posts_controller.php
@@ -20,6 +20,7 @@ class PostsController extends BaseController
         $app->map(['POST'], '/get-slug', [$this, 'get_slug']);
         $app->map(['POST'], '/upload-images', [$this, 'get_upload_images']);
         $app->map(['POST'], '/delete-image/[{id}]', [$this, 'delete_image']);
+        $app->map(['POST'], '/direct-upload', [$this, 'get_direct_upload']);
     }
 
     public function accessRules()
@@ -339,5 +340,33 @@ class PostsController extends BaseController
             echo true;
         }
         exit;
+    }
+
+    /**
+     * Direct upload image on the content of post
+     * @param $request
+     * @param $response
+     * @param $args
+     * @return mixed
+     */
+    public function get_direct_upload($request, $response, $args)
+    {
+        if ($this->_user->isGuest()){
+            return $response->withRedirect($this->_login_url);
+        }
+
+        if (isset($_FILES['file']['name'])) {
+            $path_info = pathinfo($_FILES['file']['name']);
+            if (!in_array($path_info['extension'], ['jpg','JPG','jpeg','JPEG','png','PNG'])) {
+                echo json_encode('Tipe dokumen yang diperbolehkan hanya jpg, jpeg, dan png'); exit;
+            }
+
+            $uploadfile = 'uploads/posts/' . time().'.'.$path_info['extension'];
+            move_uploaded_file($_FILES['file']['tmp_name'], $uploadfile);
+
+            echo json_encode(['location' => $this->getBaseUrl().'/'.$uploadfile]); exit;
+        }
+
+        echo json_encode('Terjadi kegagalan saat mengunggah dokumen.'); exit;
     }
 }
