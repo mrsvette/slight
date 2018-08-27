898a74632c43b3855ec02fbec74074d98c3413a6
Farid Efendi <miftahfaridse@gmail.com>
2018-08-27 22:52:07 +0700

    New sitemap and pagination

--- protected/components/tool.php
+++ protected/components/tool.php
@@ -51,4 +51,49 @@ class Tool
         }
 
     }
+
+    public function url_origin( $use_forwarded_host = false )
+    {
+        $s = $_SERVER;
+        $ssl      = ( ! empty( $s['HTTPS'] ) && $s['HTTPS'] == 'on' );
+        $sp       = strtolower( $s['SERVER_PROTOCOL'] );
+        $protocol = substr( $sp, 0, strpos( $sp, '/' ) ) . ( ( $ssl ) ? 's' : '' );
+        $port     = $s['SERVER_PORT'];
+        $port     = ( ( ! $ssl && $port=='80' ) || ( $ssl && $port=='443' ) ) ? '' : ':'.$port;
+        $host     = ( $use_forwarded_host && isset( $s['HTTP_X_FORWARDED_HOST'] ) ) ? $s['HTTP_X_FORWARDED_HOST'] : ( isset( $s['HTTP_HOST'] ) ? $s['HTTP_HOST'] : null );
+        $host     = isset( $host ) ? $host : $s['SERVER_NAME'] . $port;
+        return $protocol . '://' . $host;
+    }
+
+    public function get_sitemaps() {
+        $results = [];
+        $omodel = new \Model\OptionsModel();
+        $pages = array();
+        foreach (glob($_SERVER['DOCUMENT_ROOT'].'/themes/'.$omodel->getOptions()['theme'].'/views/*.phtml') as $filename) {
+            $page = basename($filename, '.phtml');
+            $excludes = ['post', 'sitemap.xml'];
+            if (file_exists($filename) && !in_array($page, $excludes) && strpos($page, "_") == false) {
+                $loc = self::url_origin().'/'.$page;
+                if ($page == 'index') {
+                    $loc = self::url_origin().'/';
+                }
+                $pages[] = [
+                    'loc' => $loc,
+                    'lastmod' => date ("c", filemtime($filename)),
+                    'priority' => ($page == 'index')? 1.0 : 0.5
+                ];
+            }
+        }
+        $results = array_merge($results, $pages);
+
+        $exts = $omodel->getInstalledExtensions();
+
+        if (array_key_exists("blog", $exts)) {
+            $pmodel = new \ExtensionsModel\PostModel();
+            $posts = $pmodel->getSitemaps();
+            $results = array_merge($results, $posts);
+        }
+
+        return $results;
+    }
 }
