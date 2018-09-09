b96e1c6a1d6ffa8f49f3efc28851f4d69950a12c
Farid Efendi <miftahfaridse@gmail.com>
2018-09-09 17:10:38 +0700

    Show price of the domain

--- protected/extensions/tool/components/website_tool.php
+++ protected/extensions/tool/components/website_tool.php
@@ -104,4 +104,171 @@ class WebsiteTool
 
         return false;
     }
+
+    public function get_prices($data) {
+        include_once __DIR__ . '/../../rank/simple_html_dom.php';
+
+        $dmodel = new \ExtensionsModel\DomainResellerModel();
+        $resellers = $dmodel->getRows(['enabled' => 1]);
+        $items = [];
+        foreach ($resellers as $i => $reseller) {
+            $data['reseller'] = $reseller;
+            $price_data = $this->{$reseller['code']}($data);
+            $items[$reseller['code']] = [
+                'reseller' => $reseller,
+                'price' => $price_data['price'],
+                'updated_at' => $price_data['updated_at'],
+            ];
+        }
+        return $items;
+    }
+
+    private function nia($data) {
+        $model = new \ExtensionsModel\DomainPriceModel();
+        $domain_price = $model->get_price([
+            'tld' => $data['tld'],
+            'reseller_id' => $data['reseller']['id'],
+            'hours_different' => 3
+        ]);
+
+        if ((int)$domain_price['price'] > 0) {
+            return ['price' => (int)$domain_price['price'], 'updated_at' => $domain_price['updated_at']];
+        }
+
+        $postfields = [
+            'sld' => $data['sld'],
+            'tld' => $data['tld'],
+            'transfer' => 0,
+            'unicode_sld' => $data['sld']
+        ];
+
+        $url = $data['reseller']['url'];
+        $url .= '?'.http_build_query($postfields);
+        $html = file_get_html($url);
+        $results = [];
+        foreach ($html->find('option') as $e) {
+            $val = $e->getAttribute('data-value');
+            $needle = ["rp", ".", ",", " "];
+            $replacements   = ["", "", ".", ""];
+
+            $val = str_replace($needle, $replacements, strtolower($val));
+            if ($e->getAttribute('value') == '1' || (int)$e->getAttribute('value') == 1) {
+                $results[$data['tld']] = (int)$val;
+                if ($results[$data['tld']] > 0) {
+                    // saving the price
+                    $save = $model->save_price([
+                        'tld' => $data['tld'],
+                        'reseller_id' => $data['reseller']['id'],
+                        'price' => $results[$data['tld']]
+                    ]);
+                }
+            }
+        }
+
+        return [ 'price' => $results[$data['tld']], 'updated_at' => date("Y-m-d H:i:s")];
+    }
+
+    /**
+     * @param $data
+     * @return array
+     * result original format
+     * array(13) {
+     * ["reff"]=>"domain"
+     * ["index"]=>int(1)
+     * ["domain"]=>"gsahjdhsakas.co.id"
+     * ["price"]=> "100000.00"
+     * ["period"]=>"1y"
+     * ["hostinghemat"]=>int(0)
+     * ["description"]=> ""
+     * ["type"]=>"register"
+     * ["streakprice"]=>"0.00"
+     * ["idp"]=>"0"
+     * ["maxperiod"]=>"10"
+     * ["promoperiod"]=>"0"
+     * ["url"]=>s
+     */
+    private function rmw($data) {
+        // check the data if any and still fresh
+        $model = new \ExtensionsModel\DomainPriceModel();
+        $domain_price = $model->get_price([
+            'tld' => $data['tld'],
+            'reseller_id' => $data['reseller']['id'],
+            'hours_different' => 3
+        ]);
+
+        if ((int)$domain_price['price'] > 0) {
+            return ['price' => (int)$domain_price['price'], 'updated_at' => $domain_price['updated_at']];
+        }
+
+        $url = $data['reseller']['url'];
+        $curl_type = 'POST';
+        $result_type = 'json';
+        $postfields = [
+            'registerdomain' => $data['sld'].$data['tld'],
+            'promocode' => ''
+        ];
+        //create cURL connection
+        $curl_connection = curl_init($url);
+
+        //set options
+        curl_setopt($curl_connection, CURLOPT_CONNECTTIMEOUT, 30);
+        curl_setopt($curl_connection, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($curl_connection, CURLOPT_SSL_VERIFYPEER, false);
+
+        if($curl_type == 'POST') {
+            //set data to be posted
+            curl_setopt($curl_connection, CURLOPT_USERAGENT, $_SERVER['HTTP_USER_AGENT']);
+            curl_setopt($curl_connection, CURLOPT_FOLLOWLOCATION, 1);
+            curl_setopt($curl_connection, CURLOPT_POSTFIELDS, http_build_query($postfields));
+        }else{
+            curl_setopt($curl_connection, CURLOPT_URL, $url);
+        }
+
+        //perform our request
+        $result = curl_exec($curl_connection);
+
+        //close the connection
+        curl_close($curl_connection);
+
+        if($result_type == 'json'){
+            $result = json_decode($result);
+        }
+
+        $results = $result->resultData->actionResult;
+        $items = [];
+        foreach ($results as $i => $result) {
+            if (strpos($result, "hasilpencarian(") !== false) {
+                $needle = ["hasilpencarian(", "})"];
+                $replacements   = ["", "}"];
+
+                $newphrase = str_replace($needle, $replacements, $result);
+                $newphrase = json_decode($newphrase, true);;
+                $period = $newphrase['period'];
+                if ($period == 1 || strtolower($period) == '1y') {
+                    $tld = $this->extract_tld($newphrase['domain']);
+                    $items[$tld] = (int)$newphrase['price'];
+                    if ($items[$tld] > 0) {
+                        // saving the price
+                        $save = $model->save_price([
+                            'tld' => $tld,
+                            'reseller_id' => $data['reseller']['id'],
+                            'price' => $newphrase['price']
+                        ]);
+                    }
+                }
+            }
+        }
+
+        return [ 'price' => $items[$data['tld']], 'updated_at' => date("Y-m-d H:i:s")];
+    }
+
+    private function extract_tld( $domain ) {
+        $productTLD = '';
+        $tempstr = explode(".", $domain);
+        unset($tempstr[0]);
+        foreach($tempstr as $value){
+            $productTLD = $productTLD.".".$value;
+        }
+        return $productTLD;
+    }
 }
--- protected/extensions/tool/controllers/routes.php
+++ protected/extensions/tool/controllers/routes.php
@@ -12,6 +12,10 @@ $app->post('/cek-domain', function ($request, $response, $args) {
 
     $website_tool = new \Extensions\Components\WebsiteTool();
     $is_available = $website_tool->check_availability($params);
+    $prices = [];
+    if ($is_available) {
+        $prices = $website_tool->get_prices($params);
+    }
 
     $settings = $this->get('settings');
 
@@ -21,8 +25,20 @@ $app->post('/cek-domain', function ($request, $response, $args) {
     return $this->view->render($response, 'cek_domain.phtml', [
         'tlds' => $tlds,
         'params' => $params,
-        'is_available' => $is_available
+        'is_available' => $is_available,
+        'prices' => $prices
     ]);
+});
+
+$app->get('/domain/[{name}]', function ($request, $response, $args) {
+    $params = $request->getParams();
+    if ($args['name'] == 'rumahweb') {
+        $url = 'https://order2.'.$args['name'].'.com/?domain='.$params['sld'].$params['tld'].'&reff=domain';
+    } elseif ($args['name'] == 'niagahoster') {
+        $url = 'https://panel.'.$args['name'].'.co.id/ref/7878?r=orderdomain/searchresults?sld='.$params['sld'].'&tld='.$params['tld'];
+    }
+
+    return $response->withRedirect( $url );
 });
 
 foreach (glob(__DIR__.'/*_controller.php') as $controller) {
